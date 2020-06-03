57bffafe5d73a28297adc6fcd49e72d86221e0b9
Knut Erik Giljarhus <knut.e.giljarhus@uis.no>
2020-06-03 18:22:38 +0200

    Finish documentation for airfoil module.

--- pybemt/airfoil.py
+++ pybemt/airfoil.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
+"""
+Module for holding airfoil data, and providing drag and lift coefficients to the solver.
+
+Airfoil data is stored in the folder pybemt/airfoils. Currently, the Aerodyn format
+is supported, with only a single airfoil table. Data must be available from -180 to 180 degrees,
+and a quadratic function is built to interpolate between data in the airfoil table.
+
+This module can also be executed from the command-line to plot drag and lift coefficients 
+for a single airfoil, e.g. 
+
+    >>> python airfoil.py NACA_4412
+"""
+
 import os
 import sys
 import numpy as np
@@ -8,8 +21,12 @@ from scipy.interpolate import interp1d
 from math import degrees, radians, atan2, sin, cos
 
 
-
 class Airfoil:
+    """
+    Class for storing airfoil drag and lift coefficients. Should be initialized using 
+    the load_airfoil() function.
+    """
+
     def __init__(self):
         self.name = None
         self.alpha_ = None
@@ -19,18 +36,47 @@ class Airfoil:
         self.Cd_func = None
         self.zero_lift = 0.0
         
-    def normalize_angle(self, alpha):
+    def _normalize_angle(self, alpha):
+        """
+        Ensure that the angle fulfils :math:`\pi < \alpha < \pi`
+
+        :param float alpha: Angle in radians
+        :return: Normalized angle
+        :rtype: float
+        """
+
         return atan2(sin(alpha), cos(alpha))
     
-    def Cd(self, alpha): # NB! The stored data uses degrees for the angle
-        return self.Cd_func(-self.zero_lift + degrees(self.normalize_angle(alpha)))
+    def Cd(self, alpha): 
+        """
+        Provide drag coefficent for a given angle of attack.
+
+        :param float alpha: Angle in radians
+        :return: Drag coefficient
+        :rtype: float
+        """
+        # NB! The stored data uses degrees for the angle
+        return self.Cd_func(-self.zero_lift + degrees(self._normalize_angle(alpha)))
 
     def Cl(self, alpha): # NB! The stored data uses degrees for the angle
-        return self.Cl_func(-self.zero_lift + degrees(self.normalize_angle(alpha)))
+        """
+        Provide drag coefficent for a given angle of attack.
 
-    def plot(self,col='k'):
-        pl.plot(self.alpha_, self.Cl_, col+'-')
-        pl.plot(self.alpha_, self.Cd_, col+'--')
+        :param float alpha: Angle in radians
+        :return: Drag coefficient
+        :rtype: float
+        """
+        return self.Cl_func(-self.zero_lift + degrees(self._normalize_angle(alpha)))
+
+    def plot(self, color='k'):
+        """
+        Plot lift and drag coefficients.
+
+        :param string color: Matplotlib color key. Default value is k, i.e. black.
+        """
+
+        pl.plot(self.alpha_, self.Cl_, color + '-')
+        pl.plot(self.alpha_, self.Cd_, color + '--')
         pl.title('Airfoil characteristics for ' + self.name)
         pl.xlabel('Angle of attack')
         pl.ylabel('Drag and lift coefficients')
@@ -38,7 +84,14 @@ class Airfoil:
         
         
 def load_airfoil(name):
-    
+    """
+    Load airfoil data from data file into an Airfoil object. 
+
+    :param string name: name of the airfoil to load, e.g. 'NACA_4412'.
+
+    :return: Airfoil object
+    :rtype: pybemt.Airfoil
+    """
     a = Airfoil()
     
     this_dir = os.path.dirname(os.path.abspath(__file__))
